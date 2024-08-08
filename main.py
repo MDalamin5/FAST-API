020510b123f8c620c20664f1cefe8ff84afd2821
mdalamin5 <mdal.amin5@northsouth.edu>
2024-08-08 20:22:49 +0600

    done fastapi CRUD operartion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -17,7 +17,25 @@ class Post(BaseModel):
 
 my_posts = [{'title': 'title of the post 1', 'content': 'content of the post 1', 'id': 1}, {'title': 'title of the post 2', 'content': 'content of the post 2', 'id': 2}]
 
-@app.post('/posts')
+def getSinglePost(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+        
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+@app.get("/")
+def read_root():
+    message = {
+        'name' : "Md Al Amin",
+        'age' : 22
+    }
+    return {"messages" : message}
+
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(1, 1000000)
@@ -37,18 +55,47 @@ def create_post(post: Post):
 
 # request Get method url: "/"
 
-@app.get("/")
-def read_root():
-    message = {
-        'name' : "Md Al Amin",
-        'age' : 22
-    }
-    return {"messages" : message}
+
 
 @app.get('/posts')
 def get_posts(): # path operation Function
     return {'data' : my_posts}
 
+@app.get('/posts/latest')
+def get_latest_post():
+    post = my_posts[len(my_posts) - 1]
+    return post
+
+@app.get('/posts/{id}')
+def get_post(id: int, response: Response):
+    post = getSinglePost(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+
+    return {'post_detail': post}
+
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not not exists")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put('/posts/{id}')
+def post_update(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not not exists")
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    # post_dict['title'] = post.title
+    # post_dict['content'] = post.content
+    my_posts[index] = post_dict
+    
+    return {'data' : post_dict}
 
 """
 # get request simply do: its request to retrive data form api-server
